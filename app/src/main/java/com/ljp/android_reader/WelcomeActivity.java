79f299f70a356eaa12728ac94fa1347de267ec61
JiapengLiao <liaojiapeng8@gmail.com>
2021-08-07 22:28:42 +0800

    1.修改colors文件，添加了一堆颜色；2.欢迎界面半成品

---
+++ app/src/main/java/com/ljp/android_reader/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package com.ljp.android_reader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
+        getSupportActionBar().hide();//隐藏标题栏
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
