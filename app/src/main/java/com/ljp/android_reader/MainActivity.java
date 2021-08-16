4c90644b1fa4e61d3ab27502103f6493c02c0606
JiapengLiao <liaojiapeng8@gmail.com>
2021-08-17 01:55:13 +0800

    添加了底部导航栏（BottomNavigationView）界面

--- app/src/main/java/com/ljp/android_reader/MainActivity.java
+++ app/src/main/java/com/ljp/android_reader/MainActivity.java
@@ -1,14 +1,18 @@
 package com.ljp.android_reader;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
+        getSupportActionBar().hide();//隐藏标题栏
         setContentView(R.layout.activity_main);
     }
 }
